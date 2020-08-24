040257f82d6bb2d72091f8143730b1e4f54a1e7e
Matthias Loy <matthias.loy@hbm.com>
2020-08-24 21:48:10 +0200

    Type and error handling
    Switch for message type. Handle errors first.

--- scramjet_peer.cpp
+++ scramjet_peer.cpp
@@ -34,37 +34,49 @@ static void print_version(void)
 
 void read_version_handler(const boost::system::error_code& ec, std::size_t bytes_transferred)
 {
-	if (!ec) {
-		print_version();
-	} else {
+	if (ec) {
 		std::cerr << "read version info unsucessful, ec: " << ec << std::endl;
+		return;
 	}
+
+	print_version();
 }
 
 void read_message_type_handler(const boost::system::error_code& ec, std::size_t bytes_transferred)
 {
-	if (!ec) {
-		uint8_t message_type;
-		std::memcpy(&message_type, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(message_type));
-		receive_buffer.consume(sizeof(message_type));
-		std::cout << "message_type: " << std::hex << (uint32_t)message_type << std::endl;
-
-		std::size_t bytes_in_buffer = receive_buffer.size();
-		if (bytes_in_buffer < 12) {
-			std::size_t bytes_to_read = 12 - bytes_in_buffer;
-			boost::asio::async_read(s, receive_buffer, boost::asio::transfer_at_least(bytes_to_read), read_version_handler);
-		} else {
-			print_version();
-		}
-	} else {
+	if (ec) {
 		std::cerr << "message type read unsuccessful, ec: " << ec << std::endl;
+		return;
+	}
+	uint8_t message_type;
+	std::memcpy(&message_type, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(message_type));
+	receive_buffer.consume(sizeof(message_type));
+	std::cout << "message_type: " << std::hex << (uint32_t)message_type << std::endl;
+
+	switch (message_type) {
+	/// \todo will there be an interface header from scramjet containing constants regarding the protocol
+	case 1:
+		{
+			std::size_t bytes_in_buffer = receive_buffer.size();
+			if (bytes_in_buffer < 12) {
+				std::size_t bytes_to_read = 12 - bytes_in_buffer;
+				boost::asio::async_read(s, receive_buffer, boost::asio::transfer_at_least(bytes_to_read), read_version_handler);
+			} else {
+				print_version();
+			}
+		}
+		break;
+	default:
+		// since we don't know how much to read to get behind the unknown type, we are lost here and should disconnect!
+		std::cerr << "unknown message type!" << std::endl;
+		break;
 	}
 }
 
 void connect_handler(const boost::system::error_code& ec, const boost::asio::ip::tcp::endpoint& endpoint)
 {
 	if (!ec) {
-		std::cout << "connect  successful!" << std::endl;
+		std::cout << "connect successful!" << std::endl;
 		boost::asio::async_read(s, receive_buffer, boost::asio::transfer_at_least(1), read_message_type_handler);
 	} else {
 		std::cerr << "connect unsuccessful, ec: " << ec << std::endl;
