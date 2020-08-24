d331d16eb3e6f7d16047f04c75cfd9799ebca17a
Stephan Gatzka <stephan.gatzka@gmail.com>
2020-08-24 20:03:28 +0200

    Correct reading buffer.

--- scramjet_peer.cpp
+++ scramjet_peer.cpp
@@ -1,6 +1,8 @@
-#include <cstdlib>
 #include <cstdint>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
+
 #include <endian.h>
 
 #include <boost/asio.hpp>
@@ -9,25 +11,25 @@ boost::asio::io_context io_context;
 boost::asio::ip::tcp::socket s(io_context);
 
 boost::asio::streambuf receive_buffer;
-std::istream is(&receive_buffer);
 
 static void print_version(void)
 {
 	uint32_t version_major;
-	is >> version_major;
+	std::memcpy(&version_major, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(version_major));
+	receive_buffer.consume(sizeof(version_major));
 	version_major = le32toh(version_major);
 
 	uint32_t version_minor;
-	is >> version_minor;
+	std::memcpy(&version_minor, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(version_minor));
+	receive_buffer.consume(sizeof(version_minor));
 	version_minor = le32toh(version_minor);
 
 	uint32_t version_patch;
-	is >> version_patch;
+	std::memcpy(&version_patch, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(version_patch));
+	receive_buffer.consume(sizeof(version_patch));
 	version_patch = le32toh(version_patch);
 
 	std::cout << "Protocol Version: " << std::dec << version_major << "." << version_minor << "." << version_patch << std::endl;
-
-	std::cout << "size of streambuf after reading protocol version: " << std::dec <<receive_buffer.size() << std::endl;
 }
 
 void read_version_handler(const boost::system::error_code& ec, std::size_t bytes_transferred)
@@ -43,16 +45,15 @@ void read_message_type_handler(const boost::system::error_code& ec, std::size_t 
 {
 	if (!ec) {
 		uint8_t message_type;
-		is >> message_type;
+		std::memcpy(&message_type, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(message_type));
+		receive_buffer.consume(sizeof(message_type));
 		std::cout << "message_type: " << std::hex << (uint32_t)message_type << std::endl;
-		std::cout << "size of streambuf after getting mt: " << std::dec <<receive_buffer.size() << std::endl;
 
 		std::size_t bytes_in_buffer = receive_buffer.size();
 		if (bytes_in_buffer < 12) {
 			std::size_t bytes_to_read = 12 - bytes_in_buffer;
 			boost::asio::async_read(s, receive_buffer, boost::asio::transfer_at_least(bytes_to_read), read_version_handler);
 		} else {
-			std::cout << "enough data" << std::endl;
 			print_version();
 		}
 	} else {
