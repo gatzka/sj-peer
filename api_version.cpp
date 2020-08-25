cb15b9f2f527a29c3582c32f83a3901956b266e7
Matthias Loy <matthias.loy@hbm.com>
2020-08-25 09:18:40 +0200

    naming conventions

--- api_version.cpp
+++ api_version.cpp
@@ -5,7 +5,7 @@
 
 #include "api_version.h"
 
-ApiVersion::ApiVersion(boost::asio::streambuf &receive_buffer)
+api_version::api_version(boost::asio::streambuf &receive_buffer)
 {
 	std::memcpy(&m_major, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(m_major));
 	receive_buffer.consume(sizeof(m_major));
@@ -20,8 +20,8 @@ ApiVersion::ApiVersion(boost::asio::streambuf &receive_buffer)
 	boost::endian::little_to_native_inplace(m_patch);
 }
 
-void ApiVersion::print()
+void api_version::print()
 {
-	std::cout << "Protocol Version: " << std::dec << m_major << "." << m_minor << "." << m_patch << std::endl;
+	std::cout << "API Version: " << std::dec << m_major << "." << m_minor << "." << m_patch << std::endl;
 }
 
