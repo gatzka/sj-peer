99f2bad7fa0b2acf30b4538cb43bc4e1e009a469
Stephan Gatzka <stephan.gatzka@gmail.com>
2020-08-26 07:24:38 +0200

    Fix coding style.

--- scramjet_peer.cpp
+++ scramjet_peer.cpp
@@ -40,8 +40,7 @@ boost::asio::ip::tcp::socket s(io_context);
 
 boost::asio::streambuf receive_buffer;
 
-
-void read_version_handler(const boost::system::error_code& ec, std::size_t bytes_transferred)
+void read_version_handler(const boost::system::error_code &ec, std::size_t bytes_transferred)
 {
 	if (ec) {
 		std::cerr << "read version info unsucessful, ec: " << ec << std::endl;
@@ -52,31 +51,29 @@ void read_version_handler(const boost::system::error_code& ec, std::size_t bytes
 	apiVersion.print();
 }
 
-void read_message_type_handler(const boost::system::error_code& ec, std::size_t bytes_transferred)
+void read_message_type_handler(const boost::system::error_code &ec, std::size_t bytes_transferred)
 {
 	if (ec) {
 		std::cerr << "message type read unsuccessful, ec: " << ec << std::endl;
 		return;
 	}
 	uint8_t message_type;
-	std::memcpy(&message_type, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(message_type));
+	std::memcpy(&message_type, boost::asio::buffer_cast<const void *>(receive_buffer.data()), sizeof(message_type));
 	receive_buffer.consume(sizeof(message_type));
 	std::cout << "message_type: " << std::hex << (uint32_t)message_type << std::endl;
 
 	switch (message_type) {
 	/// \todo will there be an interface header from scramjet containing constants regarding the protocol
-	case 1:
-		{
-			std::size_t bytes_in_buffer = receive_buffer.size();
-			if (bytes_in_buffer < sizeof(api_version)) {
-				std::size_t bytes_to_read = sizeof(api_version) - bytes_in_buffer;
-				boost::asio::async_read(s, receive_buffer, boost::asio::transfer_at_least(bytes_to_read), read_version_handler);
-			} else {
-				api_version apiVersion(receive_buffer);
-				apiVersion.print();
-			}
+	case 1: {
+		std::size_t bytes_in_buffer = receive_buffer.size();
+		if (bytes_in_buffer < sizeof(api_version)) {
+			std::size_t bytes_to_read = sizeof(api_version) - bytes_in_buffer;
+			boost::asio::async_read(s, receive_buffer, boost::asio::transfer_at_least(bytes_to_read), read_version_handler);
+		} else {
+			api_version apiVersion(receive_buffer);
+			apiVersion.print();
 		}
-		break;
+	} break;
 	default:
 		// since we don't know how much to read to get behind the unknown type, we are lost here and should disconnect!
 		std::cerr << "unknown message type!" << std::endl;
@@ -84,7 +81,7 @@ void read_message_type_handler(const boost::system::error_code& ec, std::size_t 
 	}
 }
 
-void connect_handler(const boost::system::error_code& ec, const boost::asio::ip::tcp::endpoint& endpoint)
+void connect_handler(const boost::system::error_code &ec, const boost::asio::ip::tcp::endpoint &endpoint)
 {
 	if (ec) {
 		std::cerr << "connect unsuccessful, ec: " << ec << std::endl;
@@ -92,7 +89,6 @@ void connect_handler(const boost::system::error_code& ec, const boost::asio::ip:
 	}
 	std::cout << "connect successful!" << std::endl;
 	boost::asio::async_read(s, receive_buffer, boost::asio::transfer_at_least(1), read_message_type_handler);
-
 }
 
 void resolve_handler(const boost::system::error_code &ec, boost::asio::ip::tcp::resolver::results_type results)
