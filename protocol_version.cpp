8fd0e0c38b45a09606c7687d20b50e0fe97a8a2a
Matthias Loy <matthias.loy@hbm.com>
2020-09-08 13:03:24 +0200

    Introduce namespace "scramjet".
    Structure stuff that belongs together

--- protocol_version.cpp
+++ protocol_version.cpp
@@ -34,22 +34,24 @@
 
 #include "protocol_version.h"
 
-protocol_version::protocol_version(boost::asio::streambuf& receive_buffer)
-{
-	std::memcpy(&m_major, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(m_major));
-	receive_buffer.consume(sizeof(m_major));
-	boost::endian::little_to_native_inplace(m_major);
+namespace scramjet {
+	protocol_version::protocol_version(boost::asio::streambuf& receive_buffer)
+	{
+		std::memcpy(&m_major, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(m_major));
+		receive_buffer.consume(sizeof(m_major));
+		boost::endian::little_to_native_inplace(m_major);
 
-	std::memcpy(&m_minor, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(m_minor));
-	receive_buffer.consume(sizeof(m_minor));
-	boost::endian::little_to_native_inplace(m_minor);
+		std::memcpy(&m_minor, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(m_minor));
+		receive_buffer.consume(sizeof(m_minor));
+		boost::endian::little_to_native_inplace(m_minor);
 
-	std::memcpy(&m_patch, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(m_patch));
-	receive_buffer.consume(sizeof(m_patch));
-	boost::endian::little_to_native_inplace(m_patch);
-}
+		std::memcpy(&m_patch, boost::asio::buffer_cast<const void*>(receive_buffer.data()), sizeof(m_patch));
+		receive_buffer.consume(sizeof(m_patch));
+		boost::endian::little_to_native_inplace(m_patch);
+	}
 
-void protocol_version::print() const
-{
-	std::cout << "Protocol version: " << std::dec << m_major << "." << m_minor << "." << m_patch << std::endl;
+	void protocol_version::print() const
+	{
+		std::cout << "Protocol version: " << std::dec << m_major << "." << m_minor << "." << m_patch << std::endl;
+	}
 }
--- protocol_version.h
+++ protocol_version.h
@@ -30,13 +30,15 @@
 
 #include <boost/asio/streambuf.hpp>
 
-class protocol_version {
-public:
-	protocol_version(boost::asio::streambuf& receive_buffer);
-	void print() const;
+namespace scramjet {
+	class protocol_version {
+	public:
+		protocol_version(boost::asio::streambuf& receive_buffer);
+		void print() const;
 
-private:
-	uint32_t m_major;
-	uint32_t m_minor;
-	uint32_t m_patch;
-};
+	private:
+		uint32_t m_major;
+		uint32_t m_minor;
+		uint32_t m_patch;
+	};
+}
